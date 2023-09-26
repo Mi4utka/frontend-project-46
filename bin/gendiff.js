d5a49ec9f86ba2a3888dc24fde44b7a1cbd83a8f
Mi4utka <mix.maisaa@mail.ru>
2023-09-26 19:39:02 +0300

    trying to make my first function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,19 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
 import { program } from 'commander';
 
-const command = (path, path2) => {
+import fs from 'fs';
+
+import * as path from 'path';
+
+const command = (filepath1, filepath2) => {
+  const object1 = filepath1.startsWith('/') ? path.resolve(filepath1) : process.cwd(filepath1);
+  const object2 = filepath2.startsWith('/') ? path.resolve(filepath2) : process.cwd(filepath2);
+  const object11 = JSON.parse(fs.readFileSync(object1));
+  const object22 = JSON.parse(fs.readFileSync(object2));
+  console.log(object11, object22);
 };
 program
   .version('0.0.1')
