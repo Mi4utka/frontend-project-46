a57285ff238ee2492a01ccf67652018ea118ffab
Mi4utka <mix.maisaa@mail.ru>
2023-10-05 12:33:30 +0300

    minot test fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const expected = readContent('expect.txt');
 test('repeat', () => {
-  expect(genDiff('__tests__/__fixtures__/file1.json', '__tests__/__fixtures__/file2.json')).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+  expect(genDiff('__tests__/__fixtures__/file1.json', '__tests__/__fixtures__/file2.json')).toEqual(expected);
 });
