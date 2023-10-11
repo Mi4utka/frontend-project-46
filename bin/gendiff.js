834ae8d61285c69050ac92c3113784064a20fdab
Mi4utka <mix.maisaa@mail.ru>
2023-10-11 19:29:54 +0300

    trying bb

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-
-/* eslint-disable no-console */
-
-import { Command } from 'commander';
+import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
 
 const program = new Command();
+
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
